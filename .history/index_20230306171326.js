3c45144dbcea8de89aaeb27ef47e2ad9acf5fe06
codinglov <112770238+codinglov@users.noreply.github.com>
2023-03-06 17:17:46 -0600

    origin

---
+++ .history/index_20230306171326.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+App from './App';
+'bootstrap'
---
+++ .history/index_20230306171448.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+importApp from './App';
+import'bootstrap/dist/css/bootstrap.min.css';
